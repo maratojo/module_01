bda03b5ab8eef98a20f8147f6e6451634d184e19
fratojo <felixfotsiny@gmail.com>
2026-03-14 14:52:25 +0300

    first commit

---
+++ ft_garden_intro.py
@@ -0,0 +1,7 @@
+def ft_garden_intro(name: str, heigh: int, age: str):
+	print("Plant:", name)
+	print(f"Heigh: {heigh}cm")
+	print("Age:", age)
+
+if __name__ == "__main__":
+	ft_garden_intro("rose", 25, "30 days")
