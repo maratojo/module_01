fa670bb7c12cd0e3a3e551d005a88f591b3d1224
maratojo <felixfotsiny@gmail.com>
2026-03-22 15:25:30 +0300

    test à vérifier

--- ft_garden_intro.py
+++ ft_garden_intro.py
@@ -3,6 +3,5 @@ def ft_garden_intro(name: str, heigh: int, age: str):
     print(f"Height: {heigh}cm")
     print("Age:", age)
 
-
 if __name__ == "__main__":
     ft_garden_intro("Rose", 25, "30 days")
