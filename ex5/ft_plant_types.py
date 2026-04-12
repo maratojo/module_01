61d2c84e8759ed46d875ed84db92536da902a31f
fratojo <felixfotsiny@gmail.com>
2026-04-12 17:41:53 +0300

    new

--- ex5/ft_plant_types.py
+++ ex5/ft_plant_types.py
@@ -6,6 +6,15 @@
 #  By: maratojo <maratojo@student.42.fr>         +#+  +:+       +#+         #
 #                                              +#+#+#+#+#+   +#+            #
 #  Created: 2026/04/12 15:06:00 by maratojo        #+#    #+#               #
-#  Updated: 2026/04/12 15:06:03 by maratojo        ###   ########.fr        #
+#  Updated: 2026/04/12 17:16:30 by maratojo        ###   ########.fr        #
 #                                                                           #
 # ************************************************************************* #
+class Plant:
+    def __init__(self, name: str, height: float, age: int):
+        self.name = name
+        self.height = height
+        self.age = age
+class Flower(Plant):
+    def __init__(self, name :str, height: float, age: int):
+        super().__init__(name, height, age)
+
